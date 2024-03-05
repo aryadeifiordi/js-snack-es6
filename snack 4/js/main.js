869b5237d07b69d3d602abf99f80124e56a8a532
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-05 15:38:39 +0100

    array

---
+++ snack 4/js/main.js
@@ -0,0 +1,12 @@
+//Crea un array di oggetti che rappresentano delle persone, ogni persona ha un nome, un cognome e un'età
+
+const persone = [
+    { nome: 'arya', cognome: 'ramesh', età: 31 },
+    { nome: 'mario', cognome: 'rossi', età: 21 },
+    { nome: 'andrea', cognome: 'veri', età: 56 },
+    { nome: 'giovanna', cognome: 'verdi', età: 17 },
+    { nome: 'michela', cognome: 'forse', età: 91 },
+    { nome: 'giovanni', cognome: 'ratti', età: 75 }
+];
+
+console.log(persone);
